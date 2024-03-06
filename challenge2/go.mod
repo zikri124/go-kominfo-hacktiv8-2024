99e059834d600f44dabf02d8ce37979002079334
zikri124 <zikriaizet@gmail.com>
2024-03-06 23:52:13 +0700

    feat(challenge 2): create directory and main.go file for challenge 2 with init project from git

---
+++ challenge2/go.mod
@@ -0,0 +1,3 @@
+module github.com/zikri124/go-kominfo-2024/challenge2
+
+go 1.22.0
---
+++ challenge2/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type User struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var users []User
+
+func main() {
+	NetHttp()
+}
+
+func NetHttp() {
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		// get all users
+		if r.Method == http.MethodGet {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(users)
+			return
+		}
+
+		// create user
+		if r.Method == http.MethodPost {
+			user := User{}
+			// only bind username and email
+			if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			user.ID = uint(len(users) + 1)
+			users = append(users, user)
+
+			w.WriteHeader(http.StatusAccepted)
+			return
+		}
+	})
+
+	// {id} => path variable
+	http.HandleFunc("/users/{id}", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			pathValue, _ := strconv.Atoi(r.PathValue("id"))
+			for _, user := range users {
+				if user.ID == uint(pathValue) {
+					w.WriteHeader(http.StatusOK)
+					json.NewEncoder(w).Encode(user)
+					return
+				}
+			}
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		// mini quiz
+		// buatlah method
+		// PUT /users/:id untuk edit user by id
+		// Delete /users/:id untuk delete user by id
+
+	})
+
+	// :8080 PORT
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Listen to port 3000")
+	}
+}
